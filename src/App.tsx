49ef497f7383c20d10d299b8f311f74a0207dccf
Jason Gunawan Limantoro <jason@ewburgers.com>
2024-10-26 03:58:19 +0800

    initial loading UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import "./App.css";
 import "@pixi/events";
-import { World } from "./components/World";
 import { WorldProvider } from "./context/World/WorldContext";
 import { useMediaQuery } from "usehooks-ts";
 import { SidebarProvider } from "./context/Sidebar/SidebarContext";
 import { EventProvider } from "./context/Event/EventsContext";
+import Loading from "./components/Loading";
+import { lazy, Suspense, useEffect, useState } from "react";
+
+const World = lazy(() => import("./components/World/World"));
 
 const worldBaseImgWidth = 4096;
 const worldBaseImgHeight = 2671;
+const loadingDuration = 5000;
 
 function App() {
   const isLargeScreen = useMediaQuery("(min-width: 1600px)");
   const isMediumScreen = useMediaQuery("(min-width: 768px)");
   const initialScale = isLargeScreen ? 1.2 : isMediumScreen ? 0.7 : 0.5;
+
+  const [progress, setProgress] = useState(0);
+
+  // Progress bar animation
+  useEffect(() => {
+    let startTime: number;
+
+    const updateProgress = (timestamp: number) => {
+      if (!startTime) startTime = timestamp;
+      const elapsed = timestamp - startTime;
+      const newProgress = Math.min((elapsed / loadingDuration) * 100, 100);
+      setProgress(newProgress);
+
+      if (elapsed < loadingDuration) {
+        requestAnimationFrame(updateProgress);
+      }
+    };
+
+    requestAnimationFrame(updateProgress);
+  }, [setProgress]);
 
   return (
     <EventProvider initialEnabled>
@@ -22,7 +46,10 @@ function App() {
         height={worldBaseImgHeight}
       >
         <SidebarProvider>
-          <World />
+          {progress < 100 && <Loading progress={progress} />}
+          <Suspense fallback={<Loading progress={progress} />}>
+            <World />
+          </Suspense>
         </SidebarProvider>
       </WorldProvider>
     </EventProvider>
